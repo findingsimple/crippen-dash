3c416edb52bdd7777001b5dc2ad6190e56a8ee56
Jason Conroy <jason@findingsimple.com>
2014-11-11 16:16:29 +1100

    Disable masonry but make event list items the same height to avoid floats catching one another

--- assets/js/crippen.js
+++ assets/js/crippen.js
@@ -13,19 +13,14 @@ var $container = $('.event-list');
 $(window).load(function() {
 
 	delay(function(){
-		// initialize Masonry after all images have loaded  
+		// initialize after all images have loaded  
 		$container.imagesLoaded( function() {
 
-		// initialize
-		$container.masonry({
-		  columnWidth: 312,
-		  gutter: 12,
-		  itemSelector: 'li'
-		});
+			equalheight('.event-list li');
 
 		});
 
-    }, 200);
+    }, 100);
 
 	/**
 	 * Make whole event panel clickable - including summary overlay
@@ -39,3 +34,44 @@ $(window).load(function() {
 });
 
 })(jQuery);
+
+/* Thanks to CSS Tricks for pointing out this bit of jQuery
+http://css-tricks.com/equal-height-blocks-in-rows/
+It's been modified into a function called at page load and then each time the page is resized. One large modification was to remove the set height before each new calculation. */
+
+equalheight = function(container){
+
+var currentTallest = 0,
+     currentRowStart = 0,
+     rowDivs = new Array(),
+     $el,
+     topPosition = 0;
+ $(container).each(function() {
+
+   $el = $(this);
+   $($el).height('auto')
+   topPostion = $el.position().top;
+
+   if (currentRowStart != topPostion) {
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+     rowDivs.length = 0; // empty the array
+     currentRowStart = topPostion;
+     currentTallest = $el.height();
+     rowDivs.push($el);
+   } else {
+     rowDivs.push($el);
+     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+  }
+   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+     rowDivs[currentDiv].height(currentTallest);
+   }
+ });
+}
+
+/**
+$(window).resize(function(){
+  equalheight('.event-list li');
+});
+**/
